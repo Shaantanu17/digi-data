30811e02d9d6f595c5403685bae93589fa47c0aa
Shaantanu17 <108970870+Shaantanu17@users.noreply.github.com>
2022-07-28 09:09:58 +0530

    Create goto.py

---
+++ goto.py
@@ -0,0 +1,13 @@
+from turtle import *
+
+pensize(3)
+penup()
+goto(-200,200)
+goto(200,200)
+pendown()
+goto(200,-200)
+write('i am shantanu',font=('arial',20,'normal'),align='center')
+goto(-200,-200)
+write('i am shantanu')
+
+mainloop()
