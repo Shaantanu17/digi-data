3dee1306bad7e6d1e3d178a0a99953ca752026ba
Shaantanu17 <108970870+Shaantanu17@users.noreply.github.com>
2022-08-04 22:45:08 +0530

    dice_simulator.py

--- basics/dice_simulator.py
+++ basics/dice_simulator.py
@@ -10,11 +10,11 @@ dice = ['1','2','3','4','5','6']
 while True:
     input('press enter to 🎲 roll dice')
     out = randint(1,2)
-    print(f'🎲=>{dice[out]}')
+    print(f'🎲=>{dice[out-1]}')
     if out == 6:
-        win_count += 1
+        win_count += 3
     elif out == 1:
-        lose_count += 1
+        lose_count += 3 
     if win_count == 3:
         print('you win 🥇')   
         break
--- uni.py
+++
@@ -1,4 +0,0 @@
-for i in range(16000):
-    print(f'{i} {chr(i)}',end='\t\t')
-    if i % 10 ==0:
-        print()
